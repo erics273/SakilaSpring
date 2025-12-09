07b55354da43e2343c38eac076ea0fb6e110ea73
Eric Schwartz <erics273@gmail.com>
2025-12-09 10:39:54 -0500

    complete add in a crude way

--- src/main/java/com/pluralsight/SakilaSpring/SakilaSpringApplication.java
+++ src/main/java/com/pluralsight/SakilaSpring/SakilaSpringApplication.java
@@ -8,6 +8,17 @@ public class SakilaSpringApplication {
 
 	public static void main(String[] args) {
 
+        //make sure the username and password were provided for the db
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar app.jar <username> <password>");
+            System.exit(1);
+        }
+
+        // Set system properties with the username and password so Spring can read them later.
+        System.setProperty("dbUsername", args[0]);
+        System.setProperty("dbPassword", args[1]);
+
+        //the spring stuff should come after the usernamen and pw stuff above
         SpringApplication.run(SakilaSpringApplication.class, args);
 
         //after the above line runs, it will any classes (components) that implement the Commnand Line Runner
